eddb892c2ebd5d19da34372a917ee3aa8a06595c
Muhammad Usman GM <muhammadusman75425965etc@gamil.com>
2025-09-30 18:11:12 +0500

    learning progress update

--- mcp6/main.py
+++ mcp6/main.py
@@ -15,16 +15,15 @@ def greet(name:str="World")-> str:
     response_message = f"Hello {name},from shared MCP server"
     return response_message
 
-
 @mcp.tool(name="get_mood",
         description="Return a user mood from the mcp server")
-@mcp.prompt(name="general_chat")
-def general_chat(user_name:str)->str:
-    return f"You are a general list agent that help {user_name} with everything."
 def greet(name:str="World")-> str:
     logger.info(f"Tool 'mood' called with name {name}")
     response_message = f"User {name} is happy"
     return response_message
-
+    
+@mcp.prompt(name="general_chat")
+def general_chat(user_name:str)->str:
+    return f"You are a general list agent that help {user_name} with everything."
 
 mcp_app = mcp.streamable_http_app()
